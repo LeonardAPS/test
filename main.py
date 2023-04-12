1683f5294ab916ee0191ab9b1dba68fbbf92032a
LeonardAPS <83495327+LeonardAPS@users.noreply.github.com>
2023-04-12 14:08:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import os
 
 def main():
-# host_id = os.environ["mysql_host"]
-
-  token = os.environ.get("MYSQL_HOST")
-    
-  print("ALL good")
-  print(token)
-
-  print("hell0")
-if __name__ == '_main__':
+  print("FUCK YEA")
+  
+if __name__ == '__main__':
     main()
 # print(host_id)
