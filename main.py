8f88e8436d06d741e3862b59202d19d26bc4c7cb
LeonardAPS <83495327+LeonardAPS@users.noreply.github.com>
2023-04-12 13:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import os
 def main():
 # host_id = os.environ["mysql_host"]
 
+  token = os.environ.get("MYSQL_HOST")
+  if not token:
+    raise RuntimeError("token not set")
+    
+  print("ALL good")
+  print(token)
+
   print("hell0")
 if __name__ == '_main__':
     main()
